1c6ed59dbf7c84dd9c44d770f6a36a6b530769b2
ggobbe <guillaume.gobbe@gmail.com>
2014-10-02 18:25:20 +0100

    Add of another sample spec file

---
+++ navigate.spec.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+describe('navigation', function () {
+    it('should allow the user to navigate in the website', function() {
+        // do some stuff
+        console.log('do some stuff');
+    });
+
+    it('should provide a map of the website', function () {
+        // do some stuff
+        console.log('do some stuff');
+    });
+});
